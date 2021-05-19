018129bd1b30dec42726b435be9015ffcd8e326a
Derick Asare <daakus@maagrace.com>
2021-05-19 11:05:58 +0000

    iinover First Commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Helper;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+        Commands\updateAllPayouts::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    
+    {
+
+        $schedule->command('update:payouts')
+        ->dailyAt('12:00');
+
+
+
+        //  $schedule->call(
+        //      function () {
+        //          Helper::updatePayouts();
+        //      }
+        //  )->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
